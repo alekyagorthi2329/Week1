0bd4bd2e852bd84720b1f8d55e2e80a8eb178f18
alekyagorthi2329 <alekyarapaka2329@gmail.com>
2026-06-04 20:08:21 +0800

    Create personal_intro.py

---
+++ personal_intro.py
@@ -0,0 +1,12 @@
+# Personal Introduction Program
+
+# Ask the user for information
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+# Display a friendly message
+print("\n🎉 Welcome,", name + "!")
+print("You are", age, "years old.")
+print("It's awesome that you enjoy", hobby + "!")
+print("Thanks for using the Personal Introduction Program 😊")
